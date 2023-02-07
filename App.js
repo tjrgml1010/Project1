66c1ee7815aa6197188d49afd56658be75c1a02a
Mat_Lee <tjrgml823@gmail.com>
2023-02-08 01:22:18 +0900

    1.0 Initialization

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View ,Button } from 'react-native';
+
 
 export default function App() {
+
+  const [cnt, setCnt] = useState(0);
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.cnt}>{cnt}</Text>
+      <Button title="더하기" onPress={ () => setCnt(cnt+1) }></Button>
+      <Button title="빼기" onPress={ () => setCnt(cnt-1) }></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  cnt:{
+    fontSize: 50,
+    marginTop: 20,
   },
 });
